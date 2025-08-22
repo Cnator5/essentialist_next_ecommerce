826251f543295896ada3651dd6399cc018743ae7
Cnator <beyoh172@gmail.com>
2025-08-22 02:02:17 +0100

    deployment errors

--- client/src/app/[category]/[subCategory]/[product]/page.jsx
+++
@@ -1,8 +0,0 @@
-// app/product/[product]/page.js
-'use client';
-
-import ProductDisplayPage from '../../../productDisplayPage/page';
-
-export default function Page({ params }) {
-  return <ProductDisplayPage params={params} />;
-}
