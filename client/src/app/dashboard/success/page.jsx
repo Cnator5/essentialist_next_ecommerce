4e2f0ab5830a7bece32e8cd34f916c5edf6048ca
Cnator <beyoh172@gmail.com>
2025-08-22 19:40:26 +0100

    useLocation

--- client/src/app/dashboard/success/page.jsx
+++ client/src/app/dashboard/success/page.jsx
@@ -1,15 +1,20 @@
 "use client"
 import React from 'react'
 import Link from 'next/link'
+import { useParams } from 'next/navigation'
 
 const Success = () => {
-  const location = useLocation()
+  const params = useParams()
+  const text = params.text
     
-    console.log("location",)  
   return (
     <div className='m-2 w-full max-w-md bg-pink-400 p-4 py-5 rounded mx-auto flex flex-col justify-center items-center gap-5'>
-        <p className='text-green-800 font-bold text-lg text-center'>{Boolean(location?.state?.text) ? location?.state?.text : "Payment" } Successfully</p>
-        <Link href="/" className="border border-green-900 text-green-900 hover:yellow-400 hover:text-white transition-all px-4 py-1">Go To Home</Link>
+        <p className='text-green-800 font-bold text-lg text-center'>
+          {text ? decodeURIComponent(text) : "Payment"} Successfully
+        </p>
+        <Link href="/" className="border border-green-900 text-green-900 hover:bg-yellow-400 hover:text-white transition-all px-4 py-1">
+          Go To Home
+        </Link>
     </div>
   )
 }
